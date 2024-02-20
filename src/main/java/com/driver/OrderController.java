32c7aaee8594814fd4cba3e9a819c00cf6e68bc6
Rohan859 <rohankundu859@gmail.com>
2024-02-20 20:02:05 +0530

    tried

--- src/main/java/com/driver/OrderController.java
+++ src/main/java/com/driver/OrderController.java
@@ -3,7 +3,6 @@ package com.driver;
 import java.util.List;
 
 import com.driver.Repository.OrderRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,8 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("orders")
 public class OrderController
 {
-    @Autowired
-    OrderRepository orderRepository;
+
+    OrderRepository orderRepository=new OrderRepository();
 
     @PostMapping("/add-order")
     public ResponseEntity<String> addOrder(@RequestBody Order order)
--- src/main/java/com/driver/Repository/OrderRepository.java
+++ src/main/java/com/driver/Repository/OrderRepository.java
@@ -2,14 +2,13 @@ package com.driver.Repository;
 
 import com.driver.DeliveryPartner;
 import com.driver.Order;
-import org.springframework.stereotype.Repository;
 
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-@Repository
+
 public class OrderRepository
 {
     HashMap<String, Order>orderDB=new HashMap<>();
